d7ef93af70be2aefbcff745fb317bff98b969935
mikemjharris <mike.mj.harris@gmail.com>
2015-01-30 21:57:24 +0000

    initialised repositry

---
+++ blog.js
@@ -0,0 +1,28 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.body.helpers({
+    posts: [
+      { 
+        title: 'post 1',
+       content: 'A big post about everything'
+      },
+      { 
+        title: 'post 2',
+       content: 'A big post about more everything'
+      }
+    ]
+  });
+
+  Template.list.userInCollection = function(){
+        return Meteor.users.find();
+    }
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+
+  });
+}
